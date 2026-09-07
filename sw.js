7535e7418c7d6dbc876cf253f5e185a55d330d75
Adam <adam.mohamed.shafik@gmail.com>
2026-09-07 03:41:37 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const CACHE = "jarvis-shell-v1";
+const SHELL = ["./", "./chat.html", "./manifest.webmanifest", "./icon.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((names) => Promise.all(names.filter((n) => n !== CACHE).map((n) => caches.delete(n))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;
+  if (e.request.method !== "GET") return;
+
+  e.respondWith(
+    caches.match(e.request).then((hit) => {
+      const live = fetch(e.request).then((res) => {
+        if (res && res.ok) {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => hit);
+      return hit || live;
+    })
+  );
+});
